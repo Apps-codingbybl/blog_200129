0b01858c2494d188e786f1232968475e147c97ac
Bobbi Lusic <46179830+codingbybl@users.noreply.github.com>
2020-02-07 20:53:42 -0800

    complete project challenges

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express = require("express");
 const request = require("request");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+// lodash for string case conversion
 const _ = require("lodash");
 const app = express();
 app.set('view engine', 'ejs');
@@ -16,11 +17,9 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. ";
 let posts = [];
 
-// create home route
+// create routes
 app.get("/", function(req,res) {
-  res.render("home", {
-    homeIntro: homeStartingContent,
-    posts: posts})
+  res.render("home", {homeIntro: homeStartingContent, posts: posts})
 });
 app.get("/about", function(req,res) {
   res.render("about", {aboutIntro: aboutContent})
@@ -33,24 +32,20 @@ app.get("/compose", function(req,res) {
 });
 
 app.post("/compose", function(req,res) {
-  // let post = req.body.postTitle + "\n" + req.body.postBody;
   const post = {
     title: req.body.postTitle,
     content: req.body.postBody
   };
   posts.push(post);
   res.redirect("/");
-  //console.log(posts);
 });
 
 app.get('/posts/:postName', function (req, res) {
-  //const requestedTitle = req.params.postName;
   const requestedTitle = _.lowerCase(req.params.postName);
 
   posts.forEach(function(post){
     const storedTitle = _.lowerCase(post.title);
     if (storedTitle === requestedTitle){
-      // console.log("Match found!");
       res.render("post", {title: post.title, content: post.content})
     }});
 });
